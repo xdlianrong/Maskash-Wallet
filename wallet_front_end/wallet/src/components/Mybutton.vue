f4ae0e7e49c30be7dd8a392f159f179c97117d7c
musJinx <musJinx18@outlook.com>
2021-01-19 18:51:18 +0800

    打算做单页面应用，迁移至vuecli ing...

---
+++ wallet_front_end/wallet/src/components/Mybutton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="button">{{ buttonMsg }}</div>
+</template>
+<style>
+    .button {
+        font-size: 1.2rem;
+        text-align: center;
+        background: #59aafa;
+        border-radius: 4px;
+        border: #f2f2f2 10px solid;
+        color: white;
+        line-height: 3.5rem;
+    }
+</style>
+<script>
+export default {
+    props: {
+        buttonMsg: {
+            type: String,
+            require: true
+        }
+    }
+}
+</script>
---
+++ wallet_front_end/wallet/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Newwallet from '../views/Newwallet.vue'
+import Main from '../views/Main.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/Newwallet',
+    name: 'Newwallet',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Newwallet
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
+
+new Vue({
+    el: '#app',
+
+})
